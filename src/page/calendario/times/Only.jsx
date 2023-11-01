e3922cb490a83ade8d3bce0a8f6976fb5bb06435
Alexander <alexdroide466@gmail.com>
2023-10-31 20:12:44 -0500

    Rediseño de las tablas

--- src/page/calendario/times/Only.jsx
+++ src/page/calendario/times/Only.jsx
@@ -1,64 +1,34 @@
 import { PiPillFill } from "react-icons/pi";
-import { BiEditAlt } from "react-icons/bi";
-import { RiDeleteBin6Fill } from "react-icons/ri";
 
 function Only() {
   return (
-    <div className="font-nunito flex gap-2">
-      <div className="flex flex-col justify-center items-center bg-green-300 p-3 px-4 text-lg font-semibold text-green-900">
-        <p className="text-center">
+    <div className="flex">
+      <div className="flex flex-col justify-center items-center bg-[#98cfba] p-3 px-[rem] text-lg font-semibold ">
+        <p className="text-center text-md text-[#649b90]">
           Only when I <br /> need it
         </p>
-        <PiPillFill className="text-[2.5rem] text-blue-950" />
+        <PiPillFill className="text-[2rem] text-[#649b90]" />
       </div>
-      <table className="table-auto border-separate">
-        <tbody className="border-2">
-          <tr className="border-2 bg-green-200 h-0">
-            <div className="flex justify-center gap-2 px-[3.6rem]">
-              <input type="checkbox" name="" id="" />
-              <div>
-                <td className="">Pepto</td>
-              </div>
-            </div>{" "}
-            <td className="px-12">500gm</td>
-            <td className="px-12">8hr</td>
-            <td className="px-10">20-10-2023</td>
-            <td className="px-10">Solo una pastilla por toma</td>
-            <div className="flex gap-5">
-              <button className="hover:bg-green-700 rounded-md">
-                <BiEditAlt className="text-green-700 hover:text-white text-3xl" />
-              </button>
-              <button className="hover:bg-red-500 rounded-md">
-                <RiDeleteBin6Fill className="text-red-500 hover:text-white text-3xl" />
-              </button>
-            </div>
-          </tr>
-          <tr className="bg-green-400 h-5">
-            <div className="flex justify-center gap-2">
-              <input type="checkbox" name="" id="" />
-              <div>
-                <td className="">Aspirina</td>
-              </div>
-            </div>
-            <td className="px-12">500gm</td>
-            <td className="px-12">8hr</td>
-            <td className="px-10">20-10-2023</td>
-            <td className="px-10">Solo una pastilla por toma</td>
-          </tr>
-          <tr className="bg-green-200 h-5">
-            <div className="flex justify-center gap-2 px-[3.4rem]">
-              <input type="checkbox" name="" id="" />
-              <div>
-                <td className="">Pepto</td>
-              </div>
-            </div>
-            <td className="px-12">500gm</td>
-            <td className="px-12">8hr</td>
-            <td className="px-10">20-10-2023</td>
-            <td className="px-10">Solo una pastilla por toma</td>
-          </tr>
-        </tbody>
-      </table>
+      <div className="font-nunito flex gap-2">
+        <table className="table-auto border-separate">
+          <tbody className="border-2">
+            <tr className="border-2 bg-[#c6ead9] text-center">
+              <td className="w-[11.45rem]">Pepto</td>
+              <td className="w-[9.4rem]">500gm</td>
+              <td className="w-[7.97rem]">8hr</td>
+              <td className="w-[10.6rem]">20-10-2023</td>
+              <td className="w-[16.8rem]">Solo una pastilla por toma</td>
+            </tr>
+            <tr className="border-2 bg-[#e4f9f3] text-center">
+              <td className="w-[11.45rem]">Pepto</td>
+              <td className="w-[9.4rem]">500gm</td>
+              <td className="w-[7.97rem]">8hr</td>
+              <td className="w-[10.6rem]">20-10-2023</td>
+              <td className="w-[16.8rem]">Solo una pastilla por toma</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
     </div>
   );
 }
